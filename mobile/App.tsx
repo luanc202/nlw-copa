98fa8e08c97cba0e8558cd3e15bb7508357c9a92
Luan <luan@luan-desktop>
2022-11-20 17:08:10 -0300

    Added AuthContext to App.tsx

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,7 +1,9 @@
 import { NativeBaseProvider, StatusBar } from "native-base";
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
-import { Sigin } from './src/screens/Signin';
+import { AuthContextProvider } from "./src/context/AuthContext";
+
+import { Pools } from './src/screens/Pools';
 import { Loading } from './src/components/Loading';
 
 import { THEME } from './src/styles/theme';
@@ -11,14 +13,16 @@ export default function App() {
 
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor='transparent'
-        translucent
-      />
+      <AuthContextProvider>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor='transparent'
+          translucent
+        />
         {
-          fontsLoaded ? <Sigin /> : <Loading /> 
+          fontsLoaded ? <Pools /> : <Loading />
         }
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
