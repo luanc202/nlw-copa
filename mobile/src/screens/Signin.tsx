b32dcaecd214344318f08e5f640c4eab0e90b58f
Luan <luan@luan-desktop>
2022-11-19 22:18:00 -0300

    Created button component for the app

--- mobile/src/screens/Signin.tsx
+++ mobile/src/screens/Signin.tsx
@@ -1,14 +1,26 @@
-import { NativeBaseProvider, Center, Text } from "native-base";
-import { THEME } from "../styles/theme";
+import { Center, Text, Icon } from "native-base";
+import { Fontisto } from '@expo/vector-icons';
+
+import Logo from '../assets/logo.svg';
+
+import { Button } from "../components/Button";
 
 export function Sigin() {
-    return (
-        <NativeBaseProvider theme={THEME}>
-            <Center flex={1} bgColor='gray.900'>
-                <Text color="white" fontSize={24} fontFamily='heading'>
-                    Hello React Native!
-                </Text>
-            </Center>
-        </NativeBaseProvider>
-    )
+  return (
+    <Center flex={1} bgColor='gray.900'>
+      <Logo width={212} height={40} />
+
+      <Button
+        title="ENTRAR COM GOOGLE"
+        leftIcon={<Icon as={Fontisto} name='google' color='white' size={'md'} />}
+        type='SECONDARY'
+        mt={12}
+      />
+
+      <Text color='white' textAlign='center' mt={4}>
+        Não utilizamos nenhuma informação além {'\n'}
+        do seu e-mail para criação de sua conta.
+      </Text>
+    </Center>
+  );
 }
